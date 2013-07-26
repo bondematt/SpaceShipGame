9cb9dac4c3472279df040cb60996375c47be6c38
bondematt <bondematt@msn.com>
2013-07-25 17:15:30 -0700

    Changes to attachement system
    Attaching now acts over several frames and walking off the collider now
    detaches character

--- Assets/Resources/Scripts/Movement Scripts/AttachToSurface.cs
+++ Assets/Resources/Scripts/Movement Scripts/AttachToSurface.cs
@@ -26,11 +26,18 @@ public class AttachToSurface : MonoBehaviour {
 	
 	Vector3 normalOfSurfaceVector = new Vector3();
 	
+	Vector3 playerFinalPosition = new Vector3();
+	
 	float facingDirection = 0f;
+	
+	GameObject normalPoint;
 	
 	// Use this for initialization
 	void Start () {
 		moveHumanoid = transform.root.GetComponentInChildren<MoveHumanoid>();
+		normalPoint = new GameObject();
+		normalPoint.name = "Reference Point";
+		normalPoint.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -38,15 +45,26 @@ public class AttachToSurface : MonoBehaviour {
 		
 		//Check if the humanoid should be attached
 		if (attach && !attached) {
+			//We are searching for a surface
 			surfaceHitInfo = CheckForSurface();
 			if (hitSurface) {
+				//we found a surface and need to attach
 				hitSurface = false;
 				Attach(surfaceHitInfo);
 			}
 		} else if (!attach && attached) {
+			//We no longer want to be attached to a surface
 			Detach();
 		} else if (attach && attached) {
-			//check if we are still on a surface
+			//check if we are still on the same surface
+			surfaceHitInfo = CheckForSurface();
+			if (hitSurface) {
+				//check if we are on same surface
+				
+			} else {
+				//No longer on a surface
+				Detach();
+			}
 		}
 		
 		//If still attaching to an object continue doing so
@@ -94,46 +112,45 @@ public class AttachToSurface : MonoBehaviour {
 		}
 	}
 	
+	/* Move player towards his final position determined by hit point and normal of surface
+	 * 
+	 * Get point in world space 1 meter in front of humanoid
+	 * Move that point up to the players 'height' above the collider.
+	 * 	- use collider.transform.InverseTransformPoint(pointDirection) 
+	 *  - Modify that new vector3.y to set the height to the same as the players
+	 *  - Then put that back into world space
+	 * rotate player to look at that new point with normal as up vector
+	 */
 	void RotateToNormal () 
 	{
 		if (rootParent != null) 
 		{
+			playerFinalPosition = hitPosition + (normalOfSurfaceVector * .9f); //Get position where we want the player to be
 			
-			/* Get point in world space 1 meter in front of humanoid
-			 * Move that point up to the players 'height' above the collider.
-			 * 	- use collider.transform.InverseTransformPoint(pointDirection) 
-			 *  - Modify that new vector3.y to set the height to the same as the players
-			 *  - Then put that back into world space
-			 * rotate player to look at that new point with normal as up vector
-			 */
+			transform.position = Vector3.Lerp(transform.position, playerFinalPosition, .05f); //Gradually move player towards this point
 			
-			/* Taskes for tomorrow
-			 * - Make it so the normalPoint always exists, just turn it off and on when needed
-			 * - Set this to take place over multiple frames while also moving the player to the correct position
-			 * - possibly allow movement while this is going on, may need to restrict it
-			 * - clean up the way this entire script functions and make it a member of MoveHumanoid as opposed to it's own component
-			 */
+			//set reference object at position of player with the rotation of the normal
+			normalPoint.SetActive(true);
 			
-			//create reference object at position of player with the rotation of the normal
-			GameObject normalPoint = new GameObject();
 			normalPoint.transform.parent = rootParent;
-			normalPoint.name = "Reference Point";
 			normalPoint.transform.position = transform.position;
-			normalPoint.transform.rotation = Quaternion.Euler (normalOfSurfaceEuler);
+			normalPoint.transform.localRotation = Quaternion.Euler (normalOfSurfaceEuler);
 			normalPoint.transform.parent = null;
 			
 			Vector3 pointDirection = transform.forward + transform.position; //world position of point 1 meter in front of player
 			
 			Vector3 offsetLocal = normalPoint.transform.InverseTransformPoint(pointDirection); //get point in reference point's local space
 			
-			offsetLocal.y = 0f; //set height to same as players
+			offsetLocal.y = Mathf.Lerp(offsetLocal.y, 0f, .05f); //set height in local space to same as player's
 			
 			pointDirection = normalPoint.transform.TransformPoint(offsetLocal); //put back into world space
 			
-			transform.LookAt(pointDirection, normalPoint.transform.up);
+			transform.LookAt(pointDirection, normalPoint.transform.up); //have player look at point with the correct up normal
 			
-			DestroyObject(normalPoint);
-			attaching = false;
+			if (offsetLocal.y == 0f && playerFinalPosition == transform.position) {
+				attaching = false; //stop attaching as we are now attached
+				normalPoint.SetActive(false); //deactivate reference point as it is no longer needed
+			}
 		} else 
 		{
 			attaching = false;
